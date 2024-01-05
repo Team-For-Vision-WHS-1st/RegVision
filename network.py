b4e31a12462bcdcbe100f48d273b23abfa78e7ff
yslim21 <yslim317@gmail.com>
2024-01-06 01:20:41 +0900

    코드수정

--- network.py
+++ network.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from Registry import Registry
 import sys
+import pandas as pd
 
 # 시스템이 사용 중인 Control set 확인
 def control_set_check(sys_reg):
@@ -20,6 +21,7 @@ def network_settings(sys_reg, soft_reg):
     Network Settings
     """
     registry = Registry.Registry(soft_reg)
+    results = []
     key = registry.open("Microsoft\\Windows NT\\CurrentVersion\\NetworkCards")
     print(("=" * 51) + "\n[+] Network Adapters\n" + ("=" * 51))
 
@@ -48,61 +50,54 @@ def network_settings(sys_reg, soft_reg):
 
         reg = Registry.Registry(sys_reg)
         key2 = reg.open("ControlSet00%s\\services\\Tcpip\\Parameters\\Interfaces" % control_set_check(sys_reg))
+        # Populate the subkeys containing the interfaces GUIDs
         int_list = []
         for v in key2.subkeys():
             int_list.append(v.name())
 
-        def guid_to_name(g):
-            for k, v in nics_dict.items():
-                '''
-                k = ServiceName, Description
-                v = GUID, Adapter name
-                '''
-                if v == g:
-                    return nics_dict['Description']
-
-       
+        # Grab the NICs info based on the above list
         for i in int_list:
-            print("[-] Interface........: %s" % guid_to_name(i))
-            print("[-] GUID.............: %s" % i)
-            key3 = reg.open("ControlSet00%s\\services\\Tcpip\\Parameters\\Interfaces\\%s" %
-                            (control_set_check(sys_reg), i))
-            for v in key3.values():
-                if v.name() == "Domain":
-                    nics_dict['Domain'] = v.value()
-                if v.name() == "IPAddress":
-                    ip = v.value()
-                    nics_dict['IPAddress'] = ip[0]
-                if v.name() == "DhcpIPAddress":
-                    nics_dict['DhcpIPAddress'] = v.value()
-                if v.name() == "DhcpServer":
-                    nics_dict['DhcpServer'] = v.value()
-                if v.name() == "DhcpSubnetMask":
-                    nics_dict['DhcpSubnetMask'] = v.value()
+            if i.startswith("{"):  # Filter out non-GUID entries
+                key3 = reg.open("ControlSet00%s\\services\\Tcpip\\Parameters\\Interfaces\\%s" %
+                                (control_set_check(sys_reg), i))
+                for v in key3.values():
+                    if v.name() == "Domain":
+                        nics_dict['Domain'] = v.value()
+                    if v.name() == "IPAddress":
+                        # Sometimes the IP would end up in a list here so just doing a little check
+                        ip = v.value()
+                        nics_dict['IPAddress'] = ip[0]
+                    if v.name() == "DhcpIPAddress":
+                        nics_dict['DhcpIPAddress'] = v.value()
+                    if v.name() == "DhcpServer":
+                        nics_dict['DhcpServer'] = v.value()
+                    if v.name() == "DhcpSubnetMask":
+                        nics_dict['DhcpSubnetMask'] = v.value()
 
-            if not 'Domain' in nics_dict:
-                nics_dict['Domain'] = "N/A"
-            if not 'IPAddress' in nics_dict:
-                nics_dict['IPAddress'] = "N/A"
-            if not 'DhcpIPAddress' in nics_dict:
-                nics_dict['DhcpIPAddress'] = "N/A"
-            if not 'DhcpServer' in nics_dict:
-                nics_dict['DhcpServer'] = "N/A"
-            if not 'DhcpSubnetMask' in nics_dict:
-                nics_dict['DhcpSubnetMask'] = "N/A"
+                # Just to avoid key errors and continue to do because not all will have these fields
+                if not 'Domain' in nics_dict:
+                    nics_dict['Domain'] = "N/A"
+                if not 'IPAddress' in nics_dict:
+                    nics_dict['IPAddress'] = "N/A"
+                if not 'DhcpIPAddress' in nics_dict:
+                    nics_dict['DhcpIPAddress'] = "N/A"
+                if not 'DhcpServer' in nics_dict:
+                    nics_dict['DhcpServer'] = "N/A"
+                if not 'DhcpSubnetMask' in nics_dict:
+                    nics_dict['DhcpSubnetMask'] = "N/A"
 
-            print("[-] Domain...........: %s" % nics_dict['Domain'])
-            print("[-] IP Address.......: %s" % nics_dict['IPAddress'])
-            print("[-] DHCP IP..........: %s" % nics_dict['DhcpIPAddress'])
-            print("[-] DHCP Server......: %s" % nics_dict['DhcpServer'])
-            print("[-] DHCP Subnet......: %s" % nics_dict['DhcpSubnetMask'])
-            print("\n")
+                results.append(nics_dict.copy())  # Copy the dictionary to avoid overwriting the same reference
+
+    return results
+
+
+
+            
 
 # hive와 software hive의 경로를 받아 네트워크 정보만 출력
 if __name__ == "__main__":
     sys_reg = sys.argv[1]
     soft_reg = sys.argv[2]
-    print("[+] SYSTEM hive:   %s" % sys_reg)
-    print("[+] SOFTWARE hive: %s" % soft_reg)
-    print("[+] The system's Control Set is :", control_set_check(sys_reg))
-    network_settings(sys_reg, soft_reg)
+    
+    results = network_settings(sys_reg, soft_reg)
+    df = pd.DataFrame(results)
